ba588936a14486be1e0032e291ac1fcb2bebc29c
PrawnCocktail <5621958+PrawnCocktail@users.noreply.github.com>
2020-05-31 02:04:23 +0100

    Added support for multiple urls, Added readme.

---
+++ README.md
@@ -0,0 +1,19 @@
+## gfxtra31 Scraper
+
+**How to use**   
+Double click the exe.    
+Enter the first page number you want to scrape, if no number is entered it will scrape from page one.    
+Enter the last page number you want to scrape, if no number is entered it will scrape all pages.    
+You can use it as command line, such as "gfxtra31.exe first=120 last=400", this will scrap pages 120 to 400.    
+ 
+**What it does**  
+- Scrapes the sitemap for all posts on the website.   
+- Dumps the page title, page url and filenext url to a CSV file.   
+- Supports multiple urls per page, seperated in the csv by a semicolon.    
+
+**What it doesn't do**  
+- Doesn't download any of the files.  
+- Doesn't scrape passwords.   
+- Doesn't check for duplicate entries.    
+
+"Hey, your code sucks!" Yup, thats why I do this for fun and not for a living.  
--- gfxtra31/Program.cs
+++ gfxtra31/Program.cs
@@ -6,8 +6,8 @@ using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace gfxtra31
 {
@@ -17,10 +17,10 @@ namespace gfxtra31
         {
             List<OutputClasses> itemList = new List<OutputClasses>();
 
-            //string url = "";
             int first = 1;
             int last = 0;
 
+            //deal with arguments here. 
             foreach (var argument in args)
             {
                 if (argument.StartsWith("first="))
@@ -37,6 +37,7 @@ namespace gfxtra31
                 }
             }
 
+            //checks and sets last page number
             int totalPages;
             if (last == 0)
             {
@@ -47,62 +48,67 @@ namespace gfxtra31
                 totalPages = last;
             }
 
-            int pageNum;
-            if (first == 1)
-            {
-                pageNum = 1;
-            }
-            else
-            {
-                pageNum = first;
-            }
+            //sets the first page number
+            int pageNum = first;
 
+            //loop though until you get to the final page, be it the total number of page or the number the user entered. 
             while (pageNum <= totalPages)
             {
-                Console.WriteLine("Processing page: " + pageNum);
-                
+                //color the new page text green so its easier to see
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("Processing page: " + pageNum);
+                Console.ResetColor();
+                Console.WriteLine();
+
                 var url = "https://www.gfxtra31.com/sitemap/page/" + pageNum;
                 var web = new HtmlWeb();
                 var doc = web.Load(url);
+
+                //gets all the results from the page, 50 per page.
                 var items = doc.DocumentNode.SelectNodes("//div[@id='mcontent_inner_box']//div[@class='quote']//a");
 
-                string pageTitle;
-                string pageUrl;
-                string fileUrl;
-
+                //loops through the results 
                 foreach (var item in items)
                 {
+                    //seperator cause why not.
+                    Console.WriteLine();
+                    Console.WriteLine("###########################################################################");
+                    Console.WriteLine();
+
                     OutputClasses itemObj = new OutputClasses();
 
-                    pageTitle = item.InnerText;
+                    //gets the page title from the website and sets in the object 
+                    string pageTitle = item.InnerText;
                     Console.WriteLine(pageTitle);
                     itemObj.Name = pageTitle.Replace(",", "");
 
-                    pageUrl = item.Attributes["href"].Value;
+                    //gets the page url from the website and sets in the object 
+                    string pageUrl = item.Attributes["href"].Value;
                     Console.WriteLine(pageUrl);
                     itemObj.PageUrl = pageUrl;
 
-                    //wait  between each item on a page. should help prevent flooding the server
-                    //Thread.Sleep(1000);
-
+                    //parses the page and gets all the filenext urls from the website and sets in the object 
                     var parseResult = parseItemPage(pageUrl);
-                    fileUrl = parseResult.Item1;
+                    string fileUrl = parseResult.Item1;
                     Console.WriteLine(fileUrl);
                     itemObj.FileUrl = fileUrl;
 
+                    //adds it to the list
                     itemList.Add(itemObj);
 
+                    //dump the list to csv every 50 records. thee is 50 on a page, so the most you can lose, should the program crash, is 1 page of records. 
+                    //this can be changed to whatever tho.
                     if (itemList.Count == 50)
                     {
-                        WriteCSV(itemList, @"items.csv");
+                        WriteCSV(itemList, @"links.csv");
                         itemList.Clear();
                     }
                 }
 
+                //increase the page number every loop.
                 pageNum++;
                 Console.WriteLine();
-                //wait 5 second between new page requests. should help prevent flooding the server
-                //Thread.Sleep(5000);
             }
 
             Console.WriteLine("Finished scraping pages " + first + " to " + totalPages);
@@ -128,21 +134,26 @@ namespace gfxtra31
             var web = new HtmlWeb();
             var doc = web.Load(url);
 
+            //used to check if the server is overloaded. if it is, it will wait to retry until it isnt overloadded
             if (doc.Text.Contains("please refresh in several seconds"))
             {
                 bool alive = false;
                 while (alive == false)
                 {
                     Console.WriteLine("Waiting to prevent overloading the site...");
-                    //sleep x seconds
+                    //this sleeps for 3 minutes, anything less and it seems to restart the server wait timer again.
                     Thread.Sleep(180000);
+
+                    //after 3 mins reload the page and recheck the message.
                     doc = web.Load(url);
                     if (doc.Text.Contains("please refresh in several seconds"))
                     {
+                        //if it contains the same message, then rerun this and wait another 3 minutes.
                         alive = false;
                     }
                     else
                     {
+                        //if not, continue on as usual.
                         alive = true;
                     }
                 }
@@ -150,8 +161,12 @@ namespace gfxtra31
 
             //website is kinda unpredictable with its layout and html tags, so we just hope that all the file urls use the go.php redirect page. 
             var items = doc.DocumentNode.SelectNodes("//a[starts-with(@href, 'https://www.gfxtra31.com/engine/go.php?url=')]");
+            List<string> pagefileurls = new List<string>();
+
+            //prevents a crash is no urls are foung. 
             if (items != null)
             {
+                //a single page can have multiple urls, so loop though them, and add them to a list.
                 foreach (var item in items)
                 {
 
@@ -160,45 +175,51 @@ namespace gfxtra31
                     string goBase = Uri.UnescapeDataString(item.Attributes["href"].Value)
                         .Replace("https://www.gfxtra31.com/engine/go.php?url=", "");
 
-                    //decode base64 string
-                    byte[] data = Convert.FromBase64String(goBase);
-                    string decodedBaseUrl = Encoding.UTF8.GetString(data);
-
-                    string fileNextUrl = "";
-                    //sometimes the url decoded is a redirect url or just the filenext url, so just check for that and handle accordingly. 
-                    if (decodedBaseUrl.Contains("filenext.com"))
+                    //some urls on the page, even though they are the "go.php" urls, constain something other than a base64 string, so check that its a base64 string
+                    if (IsBase64String(goBase))
                     {
-                        fileNextUrl = decodedBaseUrl;
-                        return Tuple.Create(fileNextUrl, "");
+                        //if is it base64 then decode the string
+                        byte[] data = Convert.FromBase64String(goBase);
+                        string decodedBaseUrl = Encoding.UTF8.GetString(data);
 
+                        string fileNextUrl = "";
+                        //sometimes the url decoded is a redirect url or just the filenext url, so just check for that and handle accordingly. 
+                        if (decodedBaseUrl.Contains("filenext.com"))
+                        {
+                            fileNextUrl = decodedBaseUrl;
+                            pagefileurls.Add(fileNextUrl);
+                        }
+                        else if (decodedBaseUrl.Contains("gftxra.net"))
+                        {
+                            //if it is a redirect url then follow and check where it redirects and use that as the filenext url. 
+                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(decodedBaseUrl);
+                            request.AllowAutoRedirect = false;
+                            var response = request.GetResponse();
+
+                            fileNextUrl = response.Headers["Location"];
+                            pagefileurls.Add(fileNextUrl);
+                        }
                     }
-                    else if (decodedBaseUrl.Contains("gftxra.net"))
-                    {
-                        //if it is a redirect url then follow and check where it redirects and use that as the filenext url. 
-                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(decodedBaseUrl);
-                        request.AllowAutoRedirect = false;
-                        var response = request.GetResponse();
 
-                        fileNextUrl = response.Headers["Location"];
-                        return Tuple.Create(fileNextUrl, "");
-
-                    }
                 }
             }
+            //not all urls on the page use the "go.php" redirection method, some are just plane filenext urls
             else if (doc.DocumentNode.SelectNodes("//a[starts-with(@href, 'https://www.filenext.com/')]") != null)
             {
-
                 var result = doc.DocumentNode.SelectNodes("//a[starts-with(@href, 'https://www.filenext.com/')]");
                 foreach (var item in result)
                 {
                     string fileNextUrl = item.Attributes["href"].Value;
-                    return Tuple.Create(fileNextUrl, "");
+                    pagefileurls.Add(fileNextUrl);
                 }
 
             }
 
-            return Tuple.Create("", "");
+            //convert the list of urls found on the page to a string seperated by a semicolon so its easier to put into the csv file.
+            string fileUrls = String.Join(";", pagefileurls);
+            return Tuple.Create(fileUrls, "");
 
+            //was gonna try and fetch the passwords on the page but its a hell of a task. the formatting is hit and miss, some pages dont have passwords and some do.
             //var passSearch = doc.DocumentNode.SelectNodes("//div[starts-with(@id, 'news-id-')]");
             //foreach (var tag in passSearch)
             //{
@@ -211,6 +232,8 @@ namespace gfxtra31
 
         static int getTotalPages(string url)
         {
+            //loads the first page of results and checks to see how many pages.
+            //this is only used if the user doesnt specify an end page 
             url = "https://www.gfxtra31.com/sitemap/page/1";
             var web = new HtmlWeb();
             var doc = web.Load(url);
@@ -237,10 +260,10 @@ namespace gfxtra31
         static void WriteCSV<T>(IEnumerable<T> items, string path)
         {
             Type itemType = typeof(T);
-            var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                .OrderBy(p => p.Name);
+            var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
             bool writeHeader = false;
+
             if (!File.Exists(path))
             {
                 writeHeader = true;
@@ -248,6 +271,7 @@ namespace gfxtra31
 
             using (var writer = new StreamWriter(path, true))
             {
+                //if csv already exists then dont write header again
                 if (writeHeader == true)
                 {
                     writer.WriteLine(string.Join(", ", props.Select(p => p.Name)));
@@ -258,6 +282,13 @@ namespace gfxtra31
                     writer.WriteLine(string.Join(", ", props.Select(p => p.GetValue(item, null))));
                 }
             }
+        }
+
+        static bool IsBase64String(string s)
+        {
+            s = s.Trim();
+            return (s.Length % 4 == 0) && Regex.IsMatch(s, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
+
         }
     }
 }
